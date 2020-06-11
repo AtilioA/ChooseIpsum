7fa8394badc9c11d307f95cbfedeb09936d06fe6
AtilioA <atiliodadalto@hotmail.com>
2020-06-11 11:23:11 -0300

    Split sentence list into three groups (lists)
    This way, generateIpsum can filter sentences according to input

--- src/utils/generateSentences.js
+++ src/utils/generateSentences.js
@@ -1,8 +1,31 @@
 import sentences from '../lib/sentences.js';
 import getSample from './getSample';
 
+import {
+  vanillaSentences,
+  swearSentences,
+  politicalSentences,
+} from '../lib/sentences';
+
+export const maxSentences =
+  vanillaSentences.length + swearSentences.length + politicalSentences.length;
+
 // Generate lorem ipsum string from n randomly chosen sentences
-export default function generateIpsum(nSentences) {
+export default function generateIpsum(
+  nSentences,
+  swear = true,
+  political = true
+) {
+  sentences = vanillaSentences;
+  if (swear) {
+    console.log('concat swear');
+    sentences = sentences.concat(swearSentences);
+  }
+  if (political) {
+    console.log('concat political');
+    sentences = sentences.concat(politicalSentences);
+  }
+
   var randomSentences = getSample(sentences, nSentences);
 
   var sentence = '';
