d45a1ad4ae81b09a3c726d7e0538ce38e66b54d9
AtilioA <atiliodadalto@hotmail.com>
2020-06-14 23:57:51 -0300

    Generate lorem ipsum

--- web/src/pages/Main/index.js
+++ web/src/pages/Main/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import generateSentences, {
+  generateParagraphs,
+} from '../../../../src/utils/generateIpsum';
 
 import { FaRandom, FaRegClipboard } from 'react-icons/fa';
 
-// import { Form, SubmitButton, List } from './styles';
-
-// import api from '../../services/api';
 import Container from '../../components/Container/index.js';
 import Footer from '../../components/Footer/index.js';
 import Header from '../../components/Header/index.js';
@@ -28,14 +27,19 @@ food into your mouth. Choose rotting away at the end of it
 all, pissing your last in a miserable home, nothing more than
 an embarrassment to the selfish, fucked-up brats you spawned
 to replace yourself.`,
+    sentences: 20,
+    paragraphs: 3,
     ipsumType: 'sentences',
     swear: true,
     political: true,
+    copySuccess: false,
   };
 
   componentDidMount() {
     const chooseIpsum = localStorage.getItem('chooseIpsum');
     const ipsumType = localStorage.getItem('ipsumType');
+    const sentences = localStorage.getItem('sentences');
+    const paragraphs = localStorage.getItem('paragraphs');
     const swear = localStorage.getItem('swear');
     const political = localStorage.getItem('political');
 
@@ -44,6 +48,12 @@ to replace yourself.`,
     }
     if (ipsumType) {
       this.setState({ ipsumType: JSON.parse(ipsumType) });
+    }
+    if (sentences) {
+      this.setState({ sentences: JSON.parse(sentences) });
+    }
+    if (paragraphs) {
+      this.setState({ paragraphs: JSON.parse(paragraphs) });
     }
     if (swear) {
       this.setState({ swear: JSON.parse(swear) });
@@ -54,13 +64,26 @@ to replace yourself.`,
   }
 
   componentDidUpdate(_, prevState) {
-    const { chooseIpsum, ipsumType, swear, political } = this.state;
+    const {
+      chooseIpsum,
+      ipsumType,
+      sentences,
+      paragraphs,
+      swear,
+      political,
+    } = this.state;
 
     if (prevState.chooseIpsum !== chooseIpsum) {
       localStorage.setItem('chooseIpsum', JSON.stringify(chooseIpsum));
     }
     if (prevState.ipsumType !== ipsumType) {
       localStorage.setItem('ipsumType', JSON.stringify(ipsumType));
+    }
+    if (prevState.sentences !== sentences) {
+      localStorage.setItem('sentences', JSON.stringify(sentences));
+    }
+    if (prevState.paragraphs !== paragraphs) {
+      localStorage.setItem('paragraphs', JSON.stringify(paragraphs));
     }
     if (prevState.swear !== swear) {
       localStorage.setItem('swear', JSON.stringify(swear));
@@ -70,32 +93,69 @@ to replace yourself.`,
     }
   }
 
+  handleClickGenerate() {
+    const { sentences, paragraphs, ipsumType, swear, political } = this.state;
+
+    console.log('oi ' + ipsumType);
+
+    switch (ipsumType) {
+      case 'sentences':
+        this.setState({
+          chooseIpsum: generateSentences(sentences, swear, political),
+        });
+        break;
+      case 'paragraphs':
+        this.setState({
+          chooseIpsum: generateParagraphs(paragraphs, swear, political),
+        });
+        break;
+    }
+
+    console.log(this.state.chooseIpsum);
+  }
+
+  handleNumberInputChange = (changeInputEvent) => {
+    this.setState({
+      sentences: changeInputEvent.currentTarget.value,
+    });
+    console.log(this.state.sentences);
+  };
+
   handleCheckboxChange(toggleCheckboxEvent) {
     this.setState({
-      [toggleCheckboxEvent.target.value]: toggleCheckboxEvent.target.checked,
+      [toggleCheckboxEvent.currentTarget.value]:
+        toggleCheckboxEvent.currentTarget.checked,
     });
     console.log({
-      [toggleCheckboxEvent.target.value]: toggleCheckboxEvent.target.checked,
+      [toggleCheckboxEvent.currentTarget.value]:
+        toggleCheckboxEvent.currentTarget.checked,
     });
   }
 
   handleRadioChange(changeRadioInputEvent) {
+    console.log(changeRadioInputEvent.currentTarget.value);
     this.setState({
-      ipsumType: changeRadioInputEvent.target.value,
+      ipsumType: changeRadioInputEvent.currentTarget.value,
     });
     console.log(this.state.ipsumType);
   }
 
+  copyToClipboard = (e) => { };
+
   render() {
-    const { chooseIpsum, swear, political } = this.state;
+    const { chooseIpsum, sentences, swear, political } = this.state;
 
     return (
       <>
         <Container>
           <Header>
-            {/* "Choose" (Number input) (Radio input) (Checkbox input) ('Generate' button) */}
             <div className="text-choose item flex-item-1">
-              Choose <input id="number-of-ipsums" type="number"></input>
+              Choose{' '}
+              <input
+                type="number"
+                defaultValue={sentences}
+                onChange={this.handleNumberInputChange}
+              ></input>
             </div>
 
             <div className="selector-choose item flex-item-1">
@@ -129,7 +189,6 @@ to replace yourself.`,
                 value="swear"
                 onChange={(e) => this.handleCheckboxChange(e)}
                 checked={swear}
-              // checked
               />
               <label htmlFor="swear">swear</label>
 
@@ -142,13 +201,12 @@ to replace yourself.`,
                 value="political"
                 checked={political}
                 onChange={(e) => this.handleCheckboxChange(e)}
-              // checked
               />
               <label htmlFor="political">political</label>
             </div>
 
             <div className="button-random item flex-item-1">
-              <button>
+              <button onClick={() => this.handleClickGenerate()}>
                 <FaRandom color={'#ccc'} /> Generate
               </button>
             </div>
@@ -157,7 +215,11 @@ to replace yourself.`,
           <Poster>
             <div className="poster-frame">
               <div className="poster-content">
-                <p>{chooseIpsum}</p>
+                <p>
+                  {Array.isArray(chooseIpsum)
+                    ? chooseIpsum.join('\n\n')
+                    : chooseIpsum}
+                </p>
                 <div className="poster-footer">
                   <h2>Choose lorem.</h2>
                   <h1>Choose ipsum.</h1>
@@ -170,7 +232,7 @@ to replace yourself.`,
           </Poster>
 
           <CopyButton>
-            <button>
+            <button onClick={() => this.copyToClipboard()}>
               <FaRegClipboard color={'#ccc'} /> Choose
             </button>
           </CopyButton>
