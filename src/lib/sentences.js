caee8e37f7834199fe7219e1ff0d210370f7e42c
AtilioA <atiliodadalto@hotmail.com>
2020-06-15 16:23:17 -0300

    Delete unused file
    Import original Choose Life text from lib/

--- src/lib/sentences.js
+++ src/lib/sentences.js
@@ -75,3 +75,18 @@ export const politicalSentences = [
   'Choose toxic masculinity. ',
   'Choose neoliberalism. ',
 ];
+
+export const chooseLifePasta = `Choose life. Choose a job. Choose a career. Choose a family.
+Choose a fucking big television. Choose washing machines,
+cars, compact disc players, and electrical tin openers. Choose
+good health, low cholesterol and dental insurance. Choose
+fixed- interest mortgage repayments. Choose a starter home.
+Choose your friends. Choose leisure wear and matching luggage.
+Choose a three-piece suit on hire purchase in a range of
+fucking fabrics. Choose DIY and wondering who the fuck you are
+on a Sunday morning. Choose sitting on that couch watching
+mind-numbing spirit-crushing game shows, stuffing fucking junk
+food into your mouth. Choose rotting away at the end of it
+all, pissing your last in a miserable home, nothing more than
+an embarrassment to the selfish, fucked-up brats you spawned
+to replace yourself.`;
--- web/src/pages/Main/index.js
+++ web/src/pages/Main/index.js
@@ -8,29 +8,18 @@ import { FaRandom, FaRegClipboard } from 'react-icons/fa';
 import generateSentences, {
   generateParagraphs,
   maxParagraphs,
-} from '../../../../src/utils/generateIpsum';
+} from '../../../../src/lib/generateIpsum';
+import { chooseLifePasta } from '../../../../src/lib/sentences';
+
 import Container from '../../components/Container/index.js';
-import Footer from '../../components/Footer/index.js';
 import Header from '../../components/Header/index.js';
-import CopyButton from '../../components/CopyButton/index.js';
 import Poster from '../../components/Poster/index.js';
+import CopyButton from '../../components/CopyButton/index.js';
+import Footer from '../../components/Footer/index.js';
 
 export default class Main extends Component {
   state = {
-    chooseIpsum: `Choose life. Choose a job. Choose a career. Choose a family.
-Choose a fucking big television. Choose washing machines,
-cars, compact disc players, and electrical tin openers. Choose
-good health, low cholesterol and dental insurance. Choose
-fixed- interest mortgage repayments. Choose a starter home.
-Choose your friends. Choose leisure wear and matching luggage.
-Choose a three-piece suit on hire purchase in a range of
-fucking fabrics. Choose DIY and wondering who the fuck you are
-on a Sunday morning. Choose sitting on that couch watching
-mind-numbing spirit-crushing game shows, stuffing fucking junk
-food into your mouth. Choose rotting away at the end of it
-all, pissing your last in a miserable home, nothing more than
-an embarrassment to the selfish, fucked-up brats you spawned
-to replace yourself.`,
+    chooseIpsum: chooseLifePasta,
     sentences: 20,
     paragraphs: 3,
     ipsumType: 'sentences',
@@ -125,7 +114,7 @@ to replace yourself.`,
           });
           break;
         default:
-          toast.error('Unknown type of ipsum.', {
+          toast.error('Unknown type of ipsum!', {
             position: 'bottom-right',
             autoClose: 5000,
             hideProgressBar: false,
@@ -148,7 +137,7 @@ to replace yourself.`,
           }
         );
         this.setState({
-          sentences: e.message, // max number of sentences depends on whether 'swear' and 'political' options are checked
+          sentences: e.message, // Max number of sentences depends on whether 'swear' and 'political' options are checked
         });
       } else if (ipsumType === 'paragraphs' && e instanceof RangeError) {
         toast.error(
@@ -167,7 +156,6 @@ to replace yourself.`,
         });
       }
     }
-
     // console.log(this.state.chooseIpsum);
   }
 
@@ -201,6 +189,7 @@ to replace yourself.`,
     console.log(this.state.ipsumType);
   }
 
+  // Generate lorem ipsum when user press Enter on input form
   handleOnKeyDownHandler = (e) => {
     if (e.keyCode === 13) {
       this.handleClickGenerate();
