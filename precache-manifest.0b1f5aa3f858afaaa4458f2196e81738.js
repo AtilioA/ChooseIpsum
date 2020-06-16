3b2bedffea576fb24f26280d780cfe4b8a208061
AtilioA <atiliodadalto@hotmail.com>
2020-06-16 16:31:29 -0300

    Updates

---
+++ precache-manifest.0b1f5aa3f858afaaa4458f2196e81738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11e413eedef3fe699d5f995945d40cc2",
+    "url": "/ChooseIpsum/index.html"
+  },
+  {
+    "revision": "d0171a0797366313167f",
+    "url": "/ChooseIpsum/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d0171a0797366313167f",
+    "url": "/ChooseIpsum/static/js/2.e24cbfb8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ChooseIpsum/static/js/2.e24cbfb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efeabcb608778548af9b",
+    "url": "/ChooseIpsum/static/js/main.6e3f823f.chunk.js"
+  },
+  {
+    "revision": "d0c3cfa56d160af7f583",
+    "url": "/ChooseIpsum/static/js/runtime-main.f9ff5432.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/*atilioa.github.io/ChooseIpsum*/precache-manifest.b8d4c50d1bfd92eaf6cb696a30de8d78.js"
+  "/ChooseIpsum/precache-manifest.0b1f5aa3f858afaaa4458f2196e81738.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/*atilioa.github.io/ChooseIpsum*/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChooseIpsum/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
